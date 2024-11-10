7d821194ddbfca1ca3338367e1d7bd4ca5bb7841
Ahamed Rizlan <arizlan@mitrai.com>
2024-11-10 22:16:40 +0530

    Tixketpool Classes added

--- src/main/java/com/example/oopcw/ticketingsystem/model/Ticketpool.java
+++ src/main/java/com/example/oopcw/ticketingsystem/model/Ticketpool.java
@@ -1,4 +1,13 @@
 package com.example.oopcw.ticketingsystem.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Ticketpool {
+    private List<Ticket> tickets;
+
+    public Ticketpool() {
+        this.tickets = Collections.synchronizedList(new ArrayList<>());
+    }
 }
